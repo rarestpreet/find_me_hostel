9d67aba89b1ebb0bf14bea8c73033a6e30662c22
arpitgoswami03 <arpit.personal.2003@gmail.com>
2025-12-28 09:14:23 +0530

    added repository layer and classes

--- src/main/java/com/project/hostel_finder/repository/CityRepo.java
+++ src/main/java/com/project/hostel_finder/repository/CityRepo.java
@@ -1,4 +1,17 @@
 package com.project.hostel_finder.repository;
 
-public interface CityRepo {
+import com.project.hostel_finder.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CityRepo extends JpaRepository<City, Long> {
+    City findByName(String name);
+
+    @Query(value = "SELECT name FROM city WHERE LOWER(name) LIKE LOWER(CONCAT('%', :keyword, '%'))", nativeQuery = true)
+    List<String> suggestions(@Param("keyword") String keyword);
 }
--- src/main/java/com/project/hostel_finder/repository/HostelRepo.java
+++ src/main/java/com/project/hostel_finder/repository/HostelRepo.java
@@ -1,4 +1,17 @@
 package com.project.hostel_finder.repository;
 
-public class HostelRepo {
+import com.project.hostel_finder.model.City;
+import com.project.hostel_finder.model.Hostel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface HostelRepo extends JpaRepository<Hostel, Long> {
+    List<Hostel> findByCity(City city);
+
+    @Query(value = "SELECT * FROM hostel WHERE LOWER(city) LIKE LOWER(CONCAT('%', :city, '%')) AND " +
+            "price <= :range", nativeQuery = true)
+    List<Hostel> findHostelsByFilters(@Param("city") String city, @Param("range") int price);
 }
